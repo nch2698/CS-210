124765ab08d8d8e41ba44720f9b49e34ae51f977
ducduyn31 <ducduy.ababat@gmail.com>
2018-10-28 22:20:14 +0700

    Update Programming challenges 3.13.cpp

--- Programming challenges 3.13.cpp
+++ Programming challenges 3.13.cpp
@@ -7,11 +7,16 @@ int main(){
 	const float STATE_RATE = 0.04f, COUNTY_RATE = 0.02f, SALE_RATE = 1.06f;
 	string time;
 	double total;
+	
 	cout << "Enter the month and the year, separated by spacebars: ";
 	getline(cin, time);
+	
 	cout << "Enter the total amount collected: ";
 	cin >> total;
-	cout << "Month: " << time << endl << "------------" << endl;
+	
+	cout << "Month: " << time << endl;
+	c<< "------------" << endl;
+	
 	cout << fixed << setprecision(2);
 	cout << left << setw(20) << "Total collected:" << '$' << right << setw(10) << total << endl;
 	cout << left << setw(20) << "Sale:" << "$" << right << setw(10) << total / SALE_RATE << endl;
