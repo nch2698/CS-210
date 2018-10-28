1ec29b13dc61c881c4c136f6cf909e9954167b2f
ducduyn31 <ducduy.ababat@gmail.com>
2018-10-28 22:29:19 +0700

    Update Programming challenges 3.18.cpp

--- Programming challenges 3.18.cpp
+++ Programming challenges 3.18.cpp
@@ -18,11 +18,14 @@ int main(){
 	m_rate = an_rate / 12.0 / 100.0;
 	p = m_rate * pow ((1 + m_rate), n) / (pow ((1 + m_rate), n) - 1) * L;
 	
-	cout << fixed << setprecision(2);
+	cout << setprecision(2);
 	
 	cout << left << setw(25) << "Loan Amount:" << '$' << right << setw(10) << L << endl;
 	cout << left << setw(25) << "Monthly Interest Rate:" << right << setw(10) << m_rate * 100.0 << '%' << endl;
 	cout << left << setw(25) << "Number of Payments:" << right << setw(11) << n << endl;
+	
+	cout << fixed;
+	
 	cout << left << setw(25) << "Monthly Payment:" << '$' << right << setw(10) << p << endl;
 	cout << left << setw(25) << "Amount Paid Back:" << '$' << right << setw(10) << p * n << endl;
 	cout << left << setw(25) << "Interest Paid:" << '$' << right << setw(10) << (p * n) - L << endl;
