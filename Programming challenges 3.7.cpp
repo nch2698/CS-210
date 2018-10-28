387e4af2ac508101e8067f4ae5335b8bb4a29e1a
ducduyn31 <ducduy.ababat@gmail.com>
2018-10-28 22:15:55 +0700

    Update Programming challenges 3.7.cpp

--- Programming challenges 3.7.cpp
+++ Programming challenges 3.7.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main(){
 	double r1, r2, r3;
 	string m1, m2, m3;
-	cout << "Enter three months to calculate monthly rainfall, separated by spacebars: ";
+	cout << "Enter three months to calculate monthly rainfall,each separated by a space: ";
 	cin >> m1 >> m2 >> m3;
-	cout << "Enter respectively the rainfall for each month in inches, separated by spacebars: ";
+	cout << "Enter respectively the rainfall for each month in inches,each separated by a space: ";
 	cin >> r1 >> r2 >> r3;
 	cout << "The average monthly rainfall for " << m1 << ", " << m2 << " and " << m3 << " was " << fixed << setprecision(2) << (r1 + r2 + r3) / 3.0 << " inches.";
 	return 0;
