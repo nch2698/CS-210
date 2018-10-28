badddd93f8e8d05b32081e36f99d9461dbb0f1c3
ducduyn31 <ducduy.ababat@gmail.com>
2018-10-28 22:17:32 +0700

    Update Programming challenges 3.11.cpp

--- Programming challenges 3.11.cpp
+++ Programming challenges 3.11.cpp
@@ -6,6 +6,7 @@ int main(){
 	double c;
 	cout << "Enter the temperature in Celsius: ";
 	cin >> c;
-	cout << "The temperature in Fahrenheit is: " << fixed << setprecision(2) << (9.0/5.0) * c + 32;
+	cout << fixed << setprecision(2);
+	cout << "The temperature in Fahrenheit is: " << (9.0/5.0) * c + 32 << " F" << endl;
 	return 0;
 }
