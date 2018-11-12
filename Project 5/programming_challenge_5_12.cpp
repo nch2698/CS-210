f9b6f3c82783319e4a9be83e122ae5ca706ee89d
ducduyn31 <ducduy.ababat@gmail.com>
2018-11-12 22:42:37 +0700

    Reformat programming challenge 5.12

--- Project 5/programming_challenge_5_12.cpp
+++ Project 5/programming_challenge_5_12.cpp
@@ -1,15 +1,16 @@
 #include<iostream>
 #include<iomanip>
+
 using namespace std;
 
-int main(){
-	int i;
-	double f;
-	cout << "Celcius" << "          Fahrenheit" << endl;
-	for(i = 1; i <= 20; i++){
-		f = i * 9 / 5.0 + 32;
-		cout << fixed << setprecision(2);
-		cout << left << setw(8) << i << right << setw(16) << f << endl;
-	}
-return 0;	
+int main() {
+    int i;
+    double f;
+    cout << "Celsius" << "          Fahrenheit" << endl;
+    for (i = 1; i <= 20; i++) {
+        f = i * 9 / 5.0 + 32;
+        cout << fixed << setprecision(2);
+        cout << left << setw(8) << i << right << setw(16) << f << endl;
+    }
+    return 0;
 }
