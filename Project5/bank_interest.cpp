eaec38778736436d87091163610883d4ba6dd3b5
Daniel Ng <ducduy.ababat@gmail.com>
2018-11-12 21:52:58 +0700

    Update Project 5 bank interest from year interest to month interest

--- Project5/bank_interest.cpp
+++ Project5/bank_interest.cpp
@@ -8,7 +8,7 @@ int main() {
 
     string name;
     double deposit, interest = 0.0;
-    float f100;
+    float fy100;
 
     cout << "Customer\'s name:\t";
     getline(cin, name);
@@ -20,19 +20,19 @@ int main() {
     } while (deposit < 0);
 
     cout << "Interest rate: (8% then enter 8)\t";
-    cin >> f100;
+    cin >> fy100;
 
     cout << "Account Name: " << name << endl;
-    cout << setw(6) << "Year" << setw(15) << "Deposit"
+    cout << setw(6) << "Month" << setw(15) << "Deposit"
          << setw(10) << "Interest" << setw(15) << "Total" << endl;
 
     cout << fixed << setprecision(0) << noshowpoint;
 
     for (int i = 1; i <= 12; i++) {
         cout << setw(6) << i << setw(15) << deposit << setw(10)
-             << deposit * f100/100 << setw(15) << deposit * (1 + f100/100) << endl;
-        deposit *= (1 + f100/100);
-        interest += deposit * f100/100;
+             << deposit * fy100/100/12 << setw(15) << deposit * (1 + fy100/12/100) << endl;
+        deposit *= (1 + fy100/12/100);
+        interest += deposit * fy100/12/100;
     }
 
     cout << "Total Interest:\t" << interest;
