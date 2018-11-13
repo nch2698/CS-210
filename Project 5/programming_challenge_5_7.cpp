077593b01482ca1ea03dea14fd978b9fdbbae6f8
ducduyn31 <ducduy.ababat@gmail.com>
2018-11-13 21:14:08 +0700

    Reformat programming challenge 5.7

--- Project 5/programming_challenge_5_7.cpp
+++ Project 5/programming_challenge_5_7.cpp
@@ -1,24 +1,26 @@
 #include<iostream>
 #include<iomanip>
+
 using namespace std;
 
-int main(){
-	double salary = 1.0, total = 0.0;
-	int day, i;
-	do {
-		cout << "The number of days? ";
-		cin >> day;
-		if (day < 1)
-			cout << "Invalid input." << endl;
-	} while (day < 1);
-	
-	cout << left << setw(5) << "Days" << "     Salary per day in dollar" << endl;
-	for (i = 1; i <= day; i++){
-		cout << fixed << setprecision(2);
-		cout << left << setw(5) << i << right << setw(20) << salary / 100 << endl;
-		total += (salary / 100);
-		salary *= 2;
-	}
-	cout << "Total" << right << setw(20) << total;
-return 0;
+int main() {
+    double salary = 1.0, total = 0.0;
+    int day, i;
+    do {
+        cout << "The number of days? ";
+        cin >> day;
+        if (day < 1)
+            cout << "Invalid input." << endl;
+    } while (day < 1);
+
+    cout << left << setw(5) << "Days" << "     Salary per day in dollar" << endl;
+    cout << fixed << setprecision(2);
+
+    for (i = 1; i <= day; i++) {
+        cout << left << setw(5) << i << right << setw(20) << salary / 100 << endl;
+        total += (salary / 100);
+        salary *= 2;
+    }
+    cout << "Total" << right << setw(20) << total;
+    return 0;
 }
