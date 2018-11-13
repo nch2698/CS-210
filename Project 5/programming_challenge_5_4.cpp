f572665d5a896d47e05cfa750c6cd84aa07ce060
ducduyn31 <ducduy.ababat@gmail.com>
2018-11-13 20:36:14 +0700

    Reformat programming challenge 5.4

--- Project 5/programming_challenge_5_4.cpp
+++ Project 5/programming_challenge_5_4.cpp
@@ -1,15 +1,17 @@
 #include<iostream>
 #include<iomanip>
+
 using namespace std;
 
-int main(){
-	const double caloriesPerMin = 3.90;
-	int min;
-	
-	cout << "Minutes" << "     Calories burnt" << endl;
-	for (min = 5; min <= 30; min +=5 ){
-		cout << fixed << setprecision(2);
-		cout << left << setw(8) << min << right << setw(15) << caloriesPerMin * min << endl;
-	}	
-return 0;
+int main() {
+    const double caloriesPerMin = 3.90;
+
+    cout << "Minutes" << "     Calories burnt" << endl;
+    cout << fixed << setprecision(2);
+    
+    for (int min = 5; min <= 30; min += 5) {
+
+        cout << left << setw(8) << min << right << setw(15) << caloriesPerMin * min << endl;
+    }
+    return 0;
 }
