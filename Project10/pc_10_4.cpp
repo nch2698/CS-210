3ec8daa5164311548efca400dcc342b0b6a55f48
Daniel Ng <ducduy.ababat@gmail.com>
2018-12-25 22:18:58 +0700

    programming challenge 10.4

--- Project10/pc_10_4.cpp
+++ Project10/pc_10_4.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <cstring>
-#include <cctype>
 
 using namespace std;
 
-int countWord(char *str) {
+double avgLetters(char *str) {
     size_t len = strlen(str);
 
     int count = 0;
+    double letters = 0.0;
     bool isCurrentWord = false;
 
     for (int i = 0; i < len; ++i) {
@@ -16,17 +16,20 @@ int countWord(char *str) {
                 isCurrentWord = true;
                 count++;
             }
-
+            letters += 1;
         } else isCurrentWord = false;
     }
 
-    return count;
+    return letters / count;
 }
 
 int main() {
     char str[256];
     cout << "Enter a string:\t";
     cin.getline(str, 256);
-    cout << "There are " << countWord(str) << " words." << endl;
+
+    cout << "The string has an average of " << avgLetters(str)
+         << " letters per word" << endl;
+
     return 0;
 }
