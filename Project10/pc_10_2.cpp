c485d0b57ff750dbcf09d1ee253a57ba3cd043f8
Daniel Ng <ducduy.ababat@gmail.com>
2018-12-25 21:55:18 +0700

    programming challenge 10.2

--- Project10/pc_10_2.cpp
+++ Project10/pc_10_2.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
+void reverse(char *str) {
+    size_t l = strlen(str);
+
+    for (int i = 0; i < l / 2; ++i) {
+        char temp = str[i];
+        str[i] = str[l - i - 1];
+        str[l - i - 1] = temp;
+    }
+
+    cout << str << endl;
+}
+
 int main() {
+    char str[100];
+    cout << "Enter a string to reverse:\t";
+    cin.getline(str, 100);
+    cout << "\t-> ";
+    reverse(str);
     return 0;
 }
