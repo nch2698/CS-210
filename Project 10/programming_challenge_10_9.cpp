2d7eac49ff9afc88e19c400673212b6ea7dea72d
ducduyn31 <ducduy.ababat@gmail.com>
2018-12-28 06:14:30 +0700

    stack memory error

--- Project 10/programming_challenge_10_9.cpp
+++ Project 10/programming_challenge_10_9.cpp
@@ -4,46 +4,48 @@
 
 using namespace std;
 
-int *appearTimes(char *arr){
-	int counter[95] = {0};
+int findMaxIndex(int l[], int size) {
 
-	for (int i = 0;i < strlen(arr); i++){
-		if(isprint(arr[i])){
-			counter[(arr[i]) - 32]++;		
-		}
-	}
-	return counter;		
+    int max = l[0], maxIndex = 0;
+
+    for (int i = 1; i < size; i++) {
+        if (max < l[i]) {
+            max = l[i];
+            maxIndex = i;
+        }
+    }
+
+    return maxIndex;
 }
 
-int *appearTimes(string arr){
-	int counter[95] = {0};
-	
-	for (int i = 0;i < arr.length(); i++){
-		if(isprint(arr[i])){
-			counter[(arr[i]) - 32]++;		
-		}
-	}
-	return counter;
+char mostFreqChar(char *arr) {
+    int counter[95] = {0};
+
+    for (int i = 0; i < strlen(arr); i++) {
+        if (isprint(arr[i])) {
+            counter[(arr[i]) - 32]++;
+        }
+    }
+    return static_cast<char>(findMaxIndex(counter, 95) + 32);
 }
+
+char mostFreqChar(string arr) {
+    int counter[95] = {0};
+
+    for (int i = 0; i < arr.length(); i++) {
+        if (isprint(arr[i])) {
+            counter[(arr[i]) - 32]++;
+        }
+    }
+    return static_cast<char>(findMaxIndex(counter, 95) + 32);
+}
+
 int main() {
     string arr;
-	int *counter;
-	
-	cout << "Enter a string: ";
-	getline(cin, arr);
-	counter = appearTimes(arr);
-	
-	int max = counter[0],
-		maxIndex;
-	int	i;
-	
-	for (i = 1; i < 95; i++){
-		if (max < counter[i]){
-			max = counter[i];
-			maxIndex = i;
-		}
-	}
-	
-	cout << "The character " << "'" << static_cast<char>(maxIndex + 32) << "'" << " with " << max << " times.";
+
+    cout << "Enter a string: ";
+    getline(cin, arr);
+
+    cout << "The character " << "'" << mostFreqChar(arr) << "'" << " appears most frequent" << endl;
     return 0;
 }
