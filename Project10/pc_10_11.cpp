a487be48349801774537d9b8d8c163b0f9ffe00a
Daniel Ng <ducduy.ababat@gmail.com>
2018-12-28 08:05:29 +0700

    10.11

--- Project10/pc_10_11.cpp
+++ Project10/pc_10_11.cpp
@@ -1,4 +1,50 @@
-//
-// Created by danielng on 26/12/2018.
-//
+#include <iostream>
+#include <cstring>
+using namespace std;
 
+//These function will not modify or change the initial array.
+void toUpper(char *);
+void toLower(char *);
+void toReverse(char *);
+
+int const SIZE = 50;
+int main(){
+    //Input string
+    char word[SIZE];
+    cout << "Enter any string (no more than " << SIZE << " characters and better with Lower case): \n";
+    cin.getline(word,SIZE);
+
+    //Output string
+    cout << "Your original string: " << word;
+    toUpper(word);
+    toLower(word);
+    toReverse(word);
+    return 0;
+}
+
+void toUpper(char *str){
+    size_t l = strlen(str);
+    char ptr[l];
+    for (int i=0;i < l;i++)
+        ptr[i] = static_cast<char>(toupper(str[i]));
+    cout << "\nYour string after call toUpper function: " << ptr;
+}
+
+void toLower(char *str){
+    size_t l = strlen(str);
+    char ptr[l];
+    for (int i=0;i < l;i++)
+        ptr[i] = static_cast<char>(tolower(str[i]));
+    cout << "\nYour string after call toLower function: " << ptr;
+}
+
+void toReverse(char *str){
+    size_t l = strlen(str);
+    char ptr[l];
+    for (int i=0;i < l;i++){
+        if (islower(str[i])) ptr[i] = static_cast<char>(toupper(str[i]));
+        else if (isupper(str[i])) ptr[i] = static_cast<char>(tolower(str[i]));
+        else ptr[i] = str[i];
+    }
+    cout << "\nYour string after call toReverse function: " << ptr;
+}
